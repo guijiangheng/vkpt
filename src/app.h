f34c724e36d10ae911b5cc68e479bf0824ca66da
guijiangheng <kakaxizhh@gmail.com>
2022-06-23 01:08:16 +0800

    feat: init commit

---
+++ src/app.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <optional>
+#include <vector>
+
+namespace vkpt {
+
+struct QueueFamilyIndices {
+  std::optional<uint32_t> graphicsFamily;
+  std::optional<uint32_t> presentFamily;
+
+  bool isComplete() { return graphicsFamily.has_value() && presentFamily.has_value(); }
+};
+
+struct SwapChainSupportDetails {
+  VkSurfaceCapabilitiesKHR capabilities;
+  std::vector<VkSurfaceFormatKHR> formats;
+  std::vector<VkPresentModeKHR> presentModes;
+};
+
+class Application {
+ public:
+  void run();
+
+ private:
+  GLFWwindow* window;
+
+  VkInstance instance;
+  VkDebugUtilsMessengerEXT debugMessenger;
+  VkSurfaceKHR surface;
+
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  VkDevice device;
+
+  VkQueue graphicsQueue;
+  VkQueue presentQueue;
+
+  VkSwapchainKHR swapChain;
+  std::vector<VkImage> swapChainImages;
+  VkFormat swapChainImageFormat;
+  VkExtent2D swapChainExtent;
+
+  void initWindow();
+  void initVulkan();
+  void mainLoop();
+  void cleanup();
+
+  void createInstance();
+  void createDebugMessenger();
+  void createSurface();
+  void pickPhysicalDevice();
+  void createLogicalDevice();
+  void createSwapChain();
+
+  bool isDeviceSuitable(VkPhysicalDevice device);
+
+  void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+
+  VkPresentModeKHR choosePresentMode(const std::vector<VkPresentModeKHR>& modes);
+  VkSurfaceFormatKHR chooseSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& formats);
+  VkExtent2D chooseExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+
+  std::vector<const char*> getRequiredExtensions();
+
+  static VKAPI_ATTR VkBool32 VKAPI_CALL
+  debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                VkDebugUtilsMessageTypeFlagsEXT messageType,
+                const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+    std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+    return VK_FALSE;
+  }
+};
+
+}  // namespace vkpt
